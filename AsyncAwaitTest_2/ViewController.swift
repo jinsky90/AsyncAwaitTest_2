449b23855ebf26966891bba26cc0d8e1b9dc627c
jinsky <sky@rainist.com>
2022-04-19 14:41:49 +0900

    Modify progress and image list

--- AsyncAwaitTest_2/ViewController.swift
+++ AsyncAwaitTest_2/ViewController.swift
@@ -16,8 +16,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var progressView: UIProgressView!
     
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+    @IBOutlet weak var imageView6: UIImageView!
+    
+    @IBOutlet weak var progressView1: UIProgressView!
+    @IBOutlet weak var progressView2: UIProgressView!
+    @IBOutlet weak var progressView3: UIProgressView!
+    @IBOutlet weak var progressView4: UIProgressView!
+    @IBOutlet weak var progressView5: UIProgressView!
+    @IBOutlet weak var progressView6: UIProgressView!
+    
+    var imageViews: [UIImageView] = []
+    var progressViews: [UIProgressView] = []
+    let imageAddresses: [URL] = [
+        URL(string:"https://cdn.pixabay.com/photo/2018/10/05/23/31/apple-3727110_1280.jpg")!, // big
+        URL(string:"https://cdn.pixabay.com/photo/2016/08/12/22/38/apple-1589874_1280.jpg")!,
+        URL(string:"https://cdn.pixabay.com/photo/2016/08/12/22/34/apple-1589869_1280.jpg")!,
+        URL(string:"https://cdn.pixabay.com/photo/2020/05/18/19/14/apple-5188076_1280.jpg")!, // big
+        URL(string:"https://cdn.pixabay.com/photo/2016/11/29/08/41/apple-1868496_1280.jpg")!,
+        URL(string:"https://cdn.pixabay.com/photo/2016/01/05/13/58/apple-1122537_1280.jpg")!,
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageViews = [imageView1, imageView2, imageView3, imageView4, imageView5, imageView6]
+        progressViews = [progressView1, progressView2, progressView3, progressView4, progressView5, progressView6]
+        
         print("메인 쓰레드 진행중 - 0")
         print("메인 쓰레드 진행중 - 1")
         
@@ -30,11 +59,11 @@ class ViewController: UIViewController {
 //            await requestImageDataBytes()
             
             // AsyncSequence + image with progress
-            let image = await requestImageInProgress(1)
-            imageView.image = image
+//            let image = await requestImageInProgress(0)
+//            imageView.image = image
             
             // AsyncSequence + Parallel
-//            await requestImageParallel()
+            await requestImageParallel()
         }
         
         print("메인 쓰레드 진행중 - 2")
@@ -87,17 +116,6 @@ class ViewController: UIViewController {
         }
     }
     
-    // MARK: - AsyncSequence + Parallel
-    // Parallel하게 async를 사용하여도 AsyncSequence인 경우 순차적으로 실행됨
-    func requestImageParallel() async {
-        async let image1 = requestImageInProgress(1)
-        async let image2 = requestImageInProgress(2)
-        async let image3 = requestImageInProgress(3)
-        async let image4 = requestImageInProgress(4)
-        
-        let imageDatum = try await [image1, image2, image3, image4]
-    }
-    
     // MARK: - Progress + image
     
     func requestImageInProgress(_ index: Int) async -> UIImage? {
@@ -120,7 +138,51 @@ class ViewController: UIViewController {
         }
     }
     
+    // MARK: - AsyncSequence + Parallel
+    func requestImageParallel() async {
+        async let image1 = requestImageInProgressWithUrl(1, url: imageAddresses[0])
+        async let image2 = requestImageInProgressWithUrl(2, url: imageAddresses[1])
+        async let image3 = requestImageInProgressWithUrl(3, url: imageAddresses[2])
+        async let image4 = requestImageInProgressWithUrl(4, url: imageAddresses[3])
+        async let image5 = requestImageInProgressWithUrl(5, url: imageAddresses[4])
+        async let image6 = requestImageInProgressWithUrl(6, url: imageAddresses[5])
+        
+        
+        let imageDatum = try await [image1, image2, image3, image4, image5, image6]
+        
+        for (index, imageData) in imageDatum.enumerated() {
+            await updateImage(image: imageData, index: index + 1)
+        }
+    }
+    
+    func requestImageInProgressWithUrl(_ index: Int, url: URL) async -> UIImage? {
+        do {
+            let (asyncBytes, urlResponse) = try await URLSession.shared.bytes(from: url)
+            let length = (urlResponse.expectedContentLength)
+            var data = Data()
+            data.reserveCapacity(Int(length))
+
+            for try await byte in asyncBytes {
+                data.append(byte)
+                let progress = Double(data.count) / Double(length)
+                await progressUpdate(progress: Float(progress), index: index)
+            }
+            print("\(index)번 이미지 다운완료")
+            return UIImage(data: data)
+        } catch {
+            return nil
+        }
+    }
+    
     @MainActor func progressUpdate(progress: Float) async {
         self.progressView.progress = progress
+    }
+    
+    @MainActor func progressUpdate(progress: Float, index: Int) async {
+        progressViews[index - 1].progress = progress
+    }
+    
+    @MainActor func updateImage(image: UIImage?, index: Int) async {
+        imageViews[index - 1].image = image
     }
 }
